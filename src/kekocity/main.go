e6e5ba9496d2098167c8ec786ab2e53236c25da0
underc0de <iegormundo@hotmail.com>
2015-04-23 23:25:04 +0200

    Add serveWs func

--- src/kekocity/main.go
+++ src/kekocity/main.go
@@ -6,48 +6,130 @@ import (
   "flag"
   "io"
   "net/http"
-  "kekocity/types"
   "github.com/gorilla/websocket"
+  "kekocity/types"
+  "time"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
 )
 
 var upgrader = websocket.Upgrader{
   ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+  WriteBufferSize: 1024,
   CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
+    return true
+  },
 }
 var addr = flag.String("addr", types.SERVICE, "http service address")
 
 type connection struct {
-	// The websocket connection.
-	ws *websocket.Conn
+  // The websocket connection
+  ws *websocket.Conn
 
-	// Buffered channel of outbound messages.
-	send chan []byte
+  // Outbound messages
+  send chan []byte
+}
+
+func (c *connection) readPump() {
+	defer func() {
+		h.unregister <- c
+		c.ws.Close()
+	}()
+	c.ws.SetReadLimit(maxMessageSize)
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+	for {
+		_, message, err := c.ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		h.broadcast <- message
+	}
+}
+
+// write writes a message with the given message type and payload.
+func (c *connection) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
+}
+
+// writePump pumps messages from the hub to the websocket connection.
+func (c *connection) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
 }
 
 func serveDefault(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found.", 404)
-		return
-	}
+  if r.URL.Path != "/" {
+    http.Error(w, "Not found.", 404)
+    return
+  }
 
+  if r.Method != "GET" {
+    http.Error(w, "Method not allowed", 405)
+    return
+  }
+
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  io.WriteString(w, "<html><body><h1>It works!</h1></body></html>")
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", 405)
 		return
 	}
-
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, "<html><body><h1>It works!</h1></body></html>")
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	c := &connection{send: make(chan []byte, 256), ws: ws}
+	h.register <- c
+	go c.writePump()
+	c.readPump()
 }
 
 func Boot() {
   flag.Parse()
 
+  go h.run()
+
   defer func() {
     // Handle functions
     http.HandleFunc("/", serveDefault)
+    http.HandleFunc("/ws", serveWs)
 
     // Start listening
     err := http.ListenAndServe(*addr, nil)
